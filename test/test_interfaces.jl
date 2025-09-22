f96e5cda14cdf5808c6a3218496760fcefb36b82
Stefan Krastanov <stefan@krastanov.org>
2025-09-22 12:55:15 +0000

    fix a bunch of tests

--- test/test_interfaces.jl
+++ test/test_interfaces.jl
@@ -6,11 +6,11 @@ using Test
 
 for (VTs, ET, _) in IGraphs.vtypes
     VT = eval(VTs)
-    Interfaces.test(BaseInterfaces.IterationInterface, VT, [VT(rand(ET,10))])
-    Interfaces.test(BaseInterfaces.IterationInterface{(:indexing)}, VT, [VT(rand(ET,10))])
+    Interfaces.test(BaseInterfaces.IterationInterface, VT, [VT(rand(ET,10))]; show=false)
+    Interfaces.test(BaseInterfaces.IterationInterface{(:indexing)}, VT, [VT(rand(ET,10))]; show=false)
 
-    Interfaces.test(BaseInterfaces.ArrayInterface, VT, [VT(rand(ET,10))])
-    Interfaces.test(BaseInterfaces.ArrayInterface{(:logical,:setindex!)}, VT, [VT(rand(ET,10))])
+    Interfaces.test(BaseInterfaces.ArrayInterface, VT, [VT(rand(ET,10))]; show=false)
+    Interfaces.test(BaseInterfaces.ArrayInterface{(:logical,:setindex!)}, VT, [VT(rand(ET,10))]; show=false)
 
     v = VT(rand(ET,10))
     @test Vector(VT(Vector(v))) == Vector(v)
@@ -19,12 +19,12 @@ end
 
 for (MTs, ET, _) in IGraphs.mtypes
     MT = eval(MTs)
-    Interfaces.test(BaseInterfaces.IterationInterface, MT, [MT(rand(ET,10,5))])
-    Interfaces.test(BaseInterfaces.IterationInterface{(:indexing)}, MT, [MT(rand(ET,10,5))])
+    Interfaces.test(BaseInterfaces.IterationInterface, MT, [MT(rand(ET,10,5))]; show=false)
+    Interfaces.test(BaseInterfaces.IterationInterface{(:indexing)}, MT, [MT(rand(ET,10,5))]; show=false)
 
 
-    Interfaces.test(BaseInterfaces.ArrayInterface, MT, [MT(rand(ET,10,5))])
-    Interfaces.test(BaseInterfaces.ArrayInterface{(:logical,:setindex!)}, MT, [MT(rand(ET,10,5))])
+    Interfaces.test(BaseInterfaces.ArrayInterface, MT, [MT(rand(ET,10,5))]; show=false)
+    Interfaces.test(BaseInterfaces.ArrayInterface{(:logical,:setindex!)}, MT, [MT(rand(ET,10,5))]; show=false)
 
     m = MT(rand(ET,10,5))
     @test Matrix(MT(Matrix(m))) == Matrix(m)
