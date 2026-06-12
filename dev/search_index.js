460f848499727fdcf26562ac9d156caf51cb78bf
Documenter.jl <documenter@juliadocs.github.io>
2026-06-12 03:09:35 +0000

    build based on 94361b1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#IGraphs.jl","page":"Home","text":"IGraphs.jl provides Julia wrappers for the igraph C library and a Graphs.jl-compatible IGraph graph type.","title":"IGraphs.jl"}]
+}
